d14e7e02cf2b00ab4191936e725f3e46c2d647ea
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-09-04 12:31:36 +0530

    vite check1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    reactRefresh(),
     vitePluginImp({
       libList: [
         {
